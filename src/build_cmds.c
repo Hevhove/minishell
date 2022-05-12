51140643e8dd796f606f3b193fd291a1edac0a64
hvan-hov <hjg.van.hove@gmail.com>
2022-05-12 13:59:44 +0100

    fixed error: redirect following pipe now no longer crashes

--- src/build_cmds.c
+++ src/build_cmds.c
@@ -6,7 +6,7 @@
 /*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 15:18:00 by Hendrik           #+#    #+#             */
-/*   Updated: 2022/05/11 19:51:35 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/05/12 13:58:43 by hvan-hov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	count_cmds(char **tokens)
 {
-	int i;
+	int	i;
 	int	cmd_no;
 
 	i = 0;
@@ -43,20 +43,18 @@ int	skip_token(char *token)
 
 int	scmd_arg_count(char **tokens, int index)
 {
-	int argc;
+	int	argc;
 
 	argc = 0;
 	if (ft_strncmp(tokens[index], "|", 1) == 0)
 		index++;
 	while (tokens[index] && ft_strncmp(tokens[index], "|", 1) != 0)
 	{
-		// careful here
 		if (ft_strncmp(tokens[index], ">", 1) == 0
 			|| ft_strncmp(tokens[index], ">>", 2) == 0
 			|| ft_strncmp(tokens[index], "<", 1) == 0
 			|| ft_strncmp(tokens[index], "<<", 2) == 0)
 				index++;
-		//printf("index is %d\n", index);
 		index++;
 		argc++;
 	}
@@ -66,11 +64,10 @@ int	scmd_arg_count(char **tokens, int index)
 int	raw_arg_count(t_cmd *cmd, char **tokens, int i, int j)
 {
 	int	count;
-	
+
 	count = 0;
 	while (tokens[i] && ft_strncmp(tokens[i], "|", 1) != 0)
 	{
-		printf("tokens[%d] is: %s\n", i, tokens[i]);
 		count++;
 		i++;
 	}
@@ -90,33 +87,36 @@ void	write_raw_cmds(t_cmd *cmd, char **tokens)
 	while (i < cmd->argc)
 	{
 		j = 0;
-		//printf("i is: %d\n", i);
-		//printf("argc is: %d\n", cmd->scmds[i].raw_argc);
-		cmd->scmds[i].raw = (char **)malloc((cmd->scmds[i].raw_argc + 1) * sizeof(char *));
+		cmd->scmds[i].raw = (char **)malloc((cmd->scmds[i].raw_argc + 1)
+				* sizeof(char *));
 		while (j < cmd->scmds[i].raw_argc)
 		{
-			//printf("tokens[k] is: %s\n", tokens[k]);
 			cmd->scmds[i].raw[j] = ft_strdup(tokens[k]);
-			//printf("raw[j] is: %s\n", cmd->scmds[i].raw[j]);
 			j++;
 			k++;
 		}
-		cmd->scmds[i].raw[j] = NULL; // DOUBLECHECK
+		cmd->scmds[i].raw[j] = NULL;
 		if (tokens[k] && ft_strncmp(tokens[k], "|", 1) == 0)
 			k++;
 		i++;
 	}
 }
 
+/*
+TODO: AT EVERY MALLOC, CHECK IF RETURN IS NOT NULL
+TODO: FREEING AFTER FAIL AND AT END OF PROGRAM?
+TODO: ERROR MANAGEMENT...
+*/
+
 void	set_cmd_count(char **tokens, t_cmd *cmd)
 {
 	int	i;
 	int	j;
-	
+
 	cmd->argc = count_cmds(tokens);
 	cmd->scmds = (t_scmd *)malloc((cmd->argc + 1) * sizeof(t_scmd));
 	if (!cmd->scmds)
-		exit(2); // error codes?
+		exit(2);
 	i = 0;
 	j = 0;
 	while (tokens[i])
@@ -126,72 +126,71 @@ void	set_cmd_count(char **tokens, t_cmd *cmd)
 			i++;
 		j++;
 	}
-	// while (tokens[i])
-	// {
-	// 	i = i + skip_token(tokens[i]);
-	// 	cmd->scmds[j].argc = scmd_arg_count(tokens, i);
-	// 	i += cmd->scmds[j++].argc;
-	// }
 }
+
+/*
+	In below function, should we still free 
+	in case of echo "hello" > outfile >> outfile2?
+	we might overwrite fname here which is a strdup causing memleak
+*/
 
 void	write_fds(t_cmd *cmd, char *fname, const char *mode, int pos)
 {
-	
 	if (ft_strncmp(mode, ">>", 2) == 0)
 	{
-		if (cmd->scmds[pos].fd_out.fname) // needed if we have multiple times the same
-			free(fname);
 		cmd->scmds[pos].fd_out.fname = ft_strdup(fname);
+		printf("fout is: %s\n", cmd->scmds[pos].fd_out.fname);
 		cmd->scmds[pos].append = 1;
 	}
 	else if (ft_strncmp(mode, "<<", 2) == 0)
 	{
-		if (cmd->scmds[pos].fd_in.fname) // needed if we have multiple times the same
-			free(fname);
 		cmd->scmds[pos].fd_in.fname = ft_strdup(fname);
 		cmd->scmds[pos].heredoc = 1;
 	}
 	else if (ft_strncmp(mode, ">", 1) == 0)
 	{
-		if (cmd->scmds[pos].fd_out.fname) // needed if we have multiple times the same
-			free(fname);
 		cmd->scmds[pos].fd_out.fname = ft_strdup(fname);
 	}
 	else if (ft_strncmp(mode, "<", 1) == 0)
 	{
-		if (cmd->scmds[pos].fd_out.fname) // needed if we have multiple times the same
-			free(fname);
 		cmd->scmds[pos].fd_in.fname = ft_strdup(fname);
 	}
+}
+
+void	init_scmd_fds(t_cmd *cmd, int i)
+{
+	cmd->scmds[i].append = 0;
+	cmd->scmds[i].heredoc = 0;
+	cmd->scmds[i].fd_in.fname = NULL;
+	cmd->scmds[i].fd_out.fname = NULL;
+	cmd->scmds[i].fd_err.fname = NULL;
 }
 
 void	update_fds(t_cmd *cmd)
 {
 	int	i;
 	int	j;
-	
-	i = 0;
-	while (i < cmd->argc)
+
+	i = -1;
+	while (++i < cmd->argc)
 	{
-		j = 0;
-		while (j < cmd->scmds[i].raw_argc)
+		j = -1;
+		init_scmd_fds(cmd, i);
+		while (++j < cmd->scmds[i].raw_argc)
 		{
-			cmd->scmds[i].append = 0;
-			cmd->scmds[i].heredoc = 0;
-			cmd->scmds[i].fd_in.fname = NULL;
-			cmd->scmds[i].fd_out.fname = NULL;
-			cmd->scmds[i].fd_err.fname = NULL;
-			if (cmd->scmds[i].raw[j] && ft_strncmp(cmd->scmds[i].raw[j], ">>", 2) == 0)
+			if (cmd->scmds[i].raw[j]
+				&& ft_strncmp(cmd->scmds[i].raw[j], ">>", 2) == 0)
 				write_fds(cmd, cmd->scmds[i].raw[++j], ">>", i);
-			else if (cmd->scmds[i].raw[j] && ft_strncmp(cmd->scmds[i].raw[j], "<<", 2) == 0)
+			else if (cmd->scmds[i].raw[j]
+				&& ft_strncmp(cmd->scmds[i].raw[j], "<<", 2) == 0)
 				write_fds(cmd, cmd->scmds[i].raw[++j], "<<", i);
-			else if (cmd->scmds[i].raw[j] && ft_strncmp(cmd->scmds[i].raw[j], ">", 1) == 0)
+			else if (cmd->scmds[i].raw[j]
+				&& ft_strncmp(cmd->scmds[i].raw[j], ">", 1) == 0)
 				write_fds(cmd, cmd->scmds[i].raw[++j], ">", i);
-			else if (cmd->scmds[i].raw[j] && ft_strncmp(cmd->scmds[i].raw[j], "<", 1) == 0)
+			else if (cmd->scmds[i].raw[j]
+				&& ft_strncmp(cmd->scmds[i].raw[j], "<", 1) == 0)
 				write_fds(cmd, cmd->scmds[i].raw[++j], "<", i);
-			j++;
 		}
-		i++;
 	}
 }
 
@@ -209,14 +208,6 @@ int	count_argv(t_scmd scmd)
 			i += 2;
 			continue ;
 		}
-		// if (ft_strncmp(scmd.raw[i], ">>", 2) == 0)
-		// 	i++;
-		// else if (ft_strncmp(scmd.raw[i], "<<", 2) == 0)
-		// 	i++;
-		// else if (ft_strncmp(scmd.raw[i], ">", 1) == 0)
-		// 	i++;
-		// else if (ft_strncmp(scmd.raw[i], "<", 1) == 0)
-		// 	i++;
 		count++;
 		i++;
 	}
@@ -228,40 +219,42 @@ void	rm_redirs(t_cmd *cmd)
 	int	i;
 	int	j;
 	int	k;
-	
-	i = 0;
-	while (i < cmd->argc)
+
+	i = -1;
+	while (++i < cmd->argc)
 	{
+		cmd->scmds[i].argc = count_argv(cmd->scmds[i]);
+		cmd->scmds[i].argv = (char **)malloc((cmd->scmds[i].argc + 1)
+				* sizeof(char *));
 		j = 0;
 		k = 0;
-		cmd->scmds[i].argc = count_argv(cmd->scmds[i]);
-		cmd->scmds[i].argv = (char **)malloc((cmd->scmds[i].argc + 1) * sizeof(char *));
 		while (j < cmd->scmds[i].raw_argc && k < cmd->scmds[i].argc)
 		{
 			if (check_token_type(cmd->scmds[i].raw[j][0]) != 1)
+			{
 				cmd->scmds[i].argv[k] = ft_strdup(cmd->scmds[i].raw[j]);
+				k++;
+			}
 			else
 				j++;
 			j++;
-			k++;
 		}
 		cmd->scmds[i].argv[k] = NULL;
-		i++;
 	}
-	// set last iteration?
 }
 
-void	build_cmds(char **tokens, t_cmd *cmd) // echo "hello $PWD"|wc -l>>outfile
+void	build_cmds(char **tokens, t_cmd *cmd)
 {
 	int	i;
-	int j;
-	
+	int	j;
+
 	set_cmd_count(tokens, cmd);
 	write_raw_cmds(cmd, tokens);
 	update_fds(cmd);
 	rm_redirs(cmd);
 	printf("cmd_arg is: %d\n", cmd->argc);
 	i = 0;
+	printf("------------\n");
 	while (i < cmd->argc)
 	{
 		j = 0;
@@ -272,11 +265,21 @@ void	build_cmds(char **tokens, t_cmd *cmd) // echo "hello $PWD"|wc -l>>outfile
 		}
 		i++;
 	}
-	printf("check fd[%d] is: %s\n", 1, cmd->scmds[1].fd_out.fname);
-	printf("check fd[%d] is: %s\n", 0, cmd->scmds[0].fd_in.fname);
+	printf("------------\n");
+	printf("STATUS CHECK: %d\n", cmd->scmds[0].append);
+	// FD CHECK
 	i = 0;
-	printf("check argv: %d\n", cmd->scmds[1].raw_argc);
-	printf("check argv: %d\n", cmd->scmds[1].argc);
+	while (i < cmd->argc)
+	{
+		printf("current fd_in [%d] is: %s\n", i, cmd->scmds[i].fd_in.fname);
+		printf("current fd_out [%d] is: %s\n", i, cmd->scmds[i].fd_out.fname);
+		printf("current fd_err [%d] is: %s\n", i, cmd->scmds[i].fd_err.fname);
+		i++;
+	}
+	printf("------------\n");
+	i = 0;
+	//printf("check argv: %d\n", cmd->scmds[1].raw_argc);
+	//printf("check argv: %d\n", cmd->scmds[1].argc);
 	while (i < cmd->argc)
 	{
 		j = 0;
