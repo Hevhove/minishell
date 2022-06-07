8c6ba5e8e5f154da47592342484d3ff6940ec41b
Mathieu Maxime fourmont <mmaxime-@c1r15s3.42lisboa.com>
2022-06-07 13:56:21 +0100

    modif pwd

--- src/pwd.c
+++ src/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvan-hov <hvan-hov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmaxime- <mmaxime-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 19:29:55 by hvan-hov          #+#    #+#             */
-/*   Updated: 2022/06/07 12:09:25 by hvan-hov         ###   ########.fr       */
+/*   Updated: 2022/06/07 13:55:53 by mmaxime-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	exec_pwd(void)
 	char	*cwd;
 
 	cwd = NULL;
-	cwd = getcwd(NULL, 0);
+	cwd = getcwd(cwd, 0);
 	if (!cwd)
 		return (-1);
-	ft_printf("%s\n", cwd);
+	ft_putstr_fd(cwd, g_cmd.scmds->fd_out.fd);
+	write(g_cmd.scmds->fd_out.fd, "\n", 1);
 	free (cwd);
 	return (0);
 }
